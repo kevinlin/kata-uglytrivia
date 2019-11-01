6c479ee18fc768f43818ed1c954c4074b4744b29
Kevin Lin <kevin.lin@zuhlke.com>
2019-11-01 16:47:25 +0800

    - Prepare game master file - Add a seed parameter to Game

--- src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -1,36 +1,50 @@
-
 package com.adaptionsoft.games.trivia.runner;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Random;
 
 import com.adaptionsoft.games.uglytrivia.Game;
 
-
 public class GameRunner {
 
-	private static boolean notAWinner;
+    private static boolean notAWinner;
 
-	public static void main(String[] args) {
-		Game aGame = new Game();
-		
-		aGame.add("Chet");
-		aGame.add("Pat");
-		aGame.add("Sue");
-		
-		Random rand = new Random();
-	
-		do {
-			
-			aGame.roll(rand.nextInt(5) + 1);
-			
-			if (rand.nextInt(9) == 7) {
-				notAWinner = aGame.wrongAnswer();
-			} else {
-				notAWinner = aGame.wasCorrectlyAnswered();
-			}
-			
-			
-			
-		} while (notAWinner);
-		
-	}
+    private final long seed;
+
+    private static int nextSeed(int index) {
+        return 13 * 19 + index;
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.setOut(new PrintStream("./game_master.txt"));
+
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("--" + i);
+            new GameRunner(nextSeed(i)).run();
+        }
+    }
+
+    GameRunner(long seed) {
+        this.seed = seed;
+    }
+
+    void run() {
+        Game aGame = new Game();
+        aGame.add("Chet");
+        aGame.add("Pat");
+        aGame.add("Sue");
+
+        Random rand = new Random(seed);
+
+        do {
+            aGame.roll(rand.nextInt(5) + 1);
+
+            if (rand.nextInt(9) == 7) {
+                notAWinner = aGame.wrongAnswer();
+            } else {
+                notAWinner = aGame.wasCorrectlyAnswered();
+            }
+        } while (notAWinner);
+
+    }
 }
